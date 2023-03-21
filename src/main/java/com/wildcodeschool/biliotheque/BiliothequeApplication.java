14205e8e604350fecc584b68576e8aebfed2a6e2
momopeace <[mohamedkadourparis@gmail.com*](mailto:mohamedkadourparis@gmail.com)>
2023-03-21 21:48:13 +0100

    finish

---
+++ src/main/java/com/wildcodeschool/biliotheque/BiliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.biliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiliothequeApplication.class, args);
+	}
+
+}
